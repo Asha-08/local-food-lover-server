8e0fa8cb9a8627128a6837dfd686f9e0a19a465a
Asha-08 <ashaamina91@gmail.com>
2025-11-11 17:36:51 +0600

    add featureReview ,ReviewDetails,AllReviews

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const app = express()
-const port = process.env.PORT || 3000
-
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const app = express();
+const port = process.env.PORT || 3000;
 
 // middleware
-app.use(cors())
-app.use(express.json())
-
+app.use(cors());
+app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.us9qyhi.mongodb.net/?appName=Cluster0`;
-
-
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -21,28 +17,46 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    
     await client.connect();
 
-    const db = client.db('local-food-lover-db')
-    const reviewCollection = db.collection('reviews')
+    const db = client.db("local-food-lover-db");
+    const reviewCollection = db.collection("reviews");
 
-    app.get ('/reviews',async (req,res)=>{
-      
-      const result = await reviewCollection.find().toArray()
-      
-      res.send(result)
+    app.get("/reviews", async (req, res) => {
+      const result = await reviewCollection
+        .find({})
+        .sort({ createdAt: -1 })
+        .toArray();
+
+      res.send(result);
+    });
+
+    // feature review
+
+    app.get("/featured-reviews", async (req, res) => {
+      const cursor = reviewCollection.find().sort({rating:-1}).limit(6);
+      const result = await cursor.toArray();
+      res.send(result);
+    });
+
+    app.get('/reviews/:id',async(req,res)=>{
+      const id = req.params.id;
+      const query = {_id:new ObjectId(id)}
+      const result = await reviewCollection.findOne(query);
+      res.send(result);
     })
 
-    
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -50,11 +64,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-app.get('/', (req, res) => {
-  res.send('Server is running fine')
-})
+app.get("/", (req, res) => {
+  res.send("Server is running fine");
+});
 
 app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`)
-})
+  console.log(`Server is listening on port ${port}`);
+});
